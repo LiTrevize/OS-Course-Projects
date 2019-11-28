18a2779afdf3f68b89daf884e859e862fc5e1aff
LiTrevize <universeofjimmy@gmail.com>
2019-11-28 11:16:01 +0800

    some modification

--- 9-contiguous-mem-alloc/src/Allocator.java
+++ 9-contiguous-mem-alloc/src/Allocator.java
@@ -2,30 +2,30 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-class Proc {
+class Seg {
     public int pid, base;
 
-    public Proc(int pid, int base) {
+    public Seg(int pid, int base) {
         this.pid = pid;
         this.base = base;
     }
 }
 
 public class Allocator {
-    private List<Proc> mem = new ArrayList<>();
+    private List<Seg> mem = new ArrayList<>();
 
     public Allocator(int size) {
         // -1 marks available memory fragment
-        mem.add(new Proc(-1, 0));
+        mem.add(new Seg(-1, 0));
         // a virtual proc mark the end of memory space
-        mem.add(new Proc(-2, size));
+        mem.add(new Seg(-2, size));
     }
 
     private int size(int i) {
         return mem.get(i + 1).base - mem.get(i).base;
     }
 
-    private int size(Proc p) {
+    private int size(Seg p) {
         int i = mem.indexOf(p);
         if (i < 0) return -1;
         return mem.get(i + 1).base - mem.get(i).base;
@@ -37,37 +37,37 @@ public class Allocator {
                 if (mem.get(i).pid == -1 && size(i) > size) {
                     int base = mem.get(i).base;
                     mem.get(i).base += size;
-                    mem.add(i, new Proc(pid, base));
+                    mem.add(i, new Seg(pid, base));
                     return 0;
                 }
             }
         }
         // Best-fit or Worst-fit
-        List<Proc> list = new ArrayList<Proc>();
-        for (Proc p : mem) {
+        List<Seg> list = new ArrayList<Seg>();
+        for (Seg p : mem) {
             if (p.pid == -1)
                 list.add(p);
         }
         if (flag == 'B')
-            list.sort(new Comparator<Proc>() {
+            list.sort(new Comparator<Seg>() {
                 @Override
-                public int compare(Proc a, Proc b) {
+                public int compare(Seg a, Seg b) {
                     return size(a) - size(b);
                 }
             });
         else if (flag == 'W')
-            list.sort(new Comparator<Proc>() {
+            list.sort(new Comparator<Seg>() {
                 @Override
-                public int compare(Proc a, Proc b) {
+                public int compare(Seg a, Seg b) {
                     return size(b) - size(a);
                 }
             });
         // search for available
-        for (Proc p : list) {
+        for (Seg p : list) {
             if (size(p) >= size) {
                 int base = p.base;
                 p.base += size;
-                mem.add(mem.indexOf(p), new Proc(pid, base));
+                mem.add(mem.indexOf(p), new Seg(pid, base));
                 return 0;
             }
         }
